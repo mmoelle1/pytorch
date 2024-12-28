1d502dd859e165215f964449be47022b8f70289d
bobrenjc93 <bobren@meta.com>
2024-12-28 13:39:25 -0800

    remove allow-untyped-defs from _export/pass_infra/proxy_value.py
    ghstack-source-id: ef28a48fd5892ae80643beb23afb339b44cebf0c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/143944

--- torch/_export/pass_infra/proxy_value.py
+++ torch/_export/pass_infra/proxy_value.py
@@ -1,13 +1,15 @@
-# mypy: allow-untyped-defs
 # pyre-strict
 from typing import Union
-
+from typing import Iterator
 import torch
 
+from typing import TypeVar
+
+_T = TypeVar("_T")
 
 class ProxyValue:
     # pyre-ignore
-    def __init__(self, data, proxy: Union[torch.fx.Proxy, torch.fx.Node]):
+    def __init__(self, data: Union[Iterator[_T], torch.Tensor], proxy: Union[torch.fx.Proxy, torch.fx.Node]):
         # pyre-ignore
         self.data = data
         self.proxy_or_node = proxy
@@ -35,7 +37,7 @@ class ProxyValue:
         return isinstance(self.data, torch.Tensor)
 
     # pyre-ignore
-    def __iter__(self):
+    def __iter__(self) -> Iterator[object]:
         yield from self.data
 
     def __bool__(self) -> bool:
